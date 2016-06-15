16f87427ace474cccbf0a039ecd73ad88a60cd3b
pinouchon <benjamin.crouzier@jobteaser.com>
2016-06-15 21:40:06 +0200

    implement #table and #select

--- filedb/lib/file_db/database.rb
+++ filedb/lib/file_db/database.rb
@@ -1,6 +1,20 @@
+require 'json'
+require 'pry'
+require 'active_support/core_ext/hash/slice'
+
 module FileDb
   class Database
     def initialize(data_file)
+      @data_file = data_file
+      @data = JSON.parse(File.read(data_file))
+    end
+
+    def table_names
+      @data.keys
+    end
+
+    def table(table_name)
+      FileDb::Table.new(table_name, @data[table_name])
     end
   end
 end

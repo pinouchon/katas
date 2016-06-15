7f637f8ee8cdd6103980dac0eea102084de77401
pinouchon <benjamin.crouzier@jobteaser.com>
2016-06-15 21:50:36 +0200

    oops

--- filedb/lib/file_db/table.rb
+++ filedb/lib/file_db/table.rb
@@ -1,4 +1,25 @@
 module FileDb
   class Table
+    def initialize(table_name, table_data)
+      @table_name = table_name
+      @table_data = table_data
+    end
+
+    def select(params)
+      result = @table_data
+
+      if params[:where]
+        params[:where].each do |field, value|
+          result = result.select { |row| row[field.to_s] == value }
+        end
+      end
+
+      if params[:fields]
+        result = result.map { |row| row.slice(*params[:fields].map(&:to_s)) }
+      end
+
+      result
+
+    end
   end
 end
